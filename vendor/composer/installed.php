21122897edd16d6e131602ff48f2b1e22d6ff36b
Costin Ionut <51002637+Staark94@users.noreply.github.com>
2021-06-30 13:52:23 +0300

    testing

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ae33a4a4bb78eef07be41f0e8c96e29e0584b6a5',
+        'name' => 'staark/user',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'staark/user' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ae33a4a4bb78eef07be41f0e8c96e29e0584b6a5',
+            'dev_requirement' => false,
+        ),
+    ),
+);
